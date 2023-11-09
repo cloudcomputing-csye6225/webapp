b07e34c232a3b9199aaa4ab6fa627c9de6418408
DrishtiGoda <goda.d@northeastern.edu>
2023-11-08 19:27:12 -0500

    Handled text condition in API

--- api/routes/connection-router.js
+++ api/routes/connection-router.js
@@ -1,12 +1,14 @@
 import express from "express";
 import * as connectionController from "../controllers/connection-controller.js";
 import { setResponse } from "../utils/response.js";
+import {checkPayloadBody} from "../utils/payload.js";
+
 
 const router = express.Router();
 
 router
   .route("/")
-  .get(connectionController.checkConnection)
+  .get(checkPayloadBody, connectionController.checkConnection)
   .all((req, res) => {
     setResponse(req,res, 405);
   });
