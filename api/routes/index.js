f1259c09dee845fa33664e4c86b7393596a6283a
DrishtiGoda <goda.d@northeastern.edu>
2023-12-07 13:27:40 -0500

    Assignment 10 demo

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,7 +7,7 @@ import { checkHealth } from "../utils/check-health.js";
 export default (app) => {
   app.use("/healthz", connectionRouter);
 
-  app.use("/demo/assignments",checkHealth, authenticate, assignmentRouter);
+  app.use("/v3/assignments",checkHealth, authenticate, assignmentRouter);
 
   app.use((req, res) => {
     setResponse(req,res, 404);
